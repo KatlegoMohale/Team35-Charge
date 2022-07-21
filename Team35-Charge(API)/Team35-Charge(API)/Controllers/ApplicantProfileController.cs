7e9bd6b7d37d91157e38cea6ae0f5bc53e513922
IamTshepiso <97446777+IamTshepiso@users.noreply.github.com>
2022-07-21 15:28:02 +0200

    controller

---
+++ Team35-Charge(API)/Team35-Charge(API)/Controllers/ApplicantProfileController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Team35_Charge_API_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicantProfileController : ControllerBase
+    {
+    }
+}
